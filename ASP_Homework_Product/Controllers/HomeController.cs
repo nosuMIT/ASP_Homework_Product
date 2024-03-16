1480526f12c6c4936eba9807a7858bb8f2f7551f
OlegEKX <olegjarl@gmail.com>
2024-03-16 22:42:14 +0300

    Исправил свое решение по видео-разбору (мое решение коммитом раньше)

--- ASP_Homework_Product/Controllers/HomeController.cs
+++ ASP_Homework_Product/Controllers/HomeController.cs
@@ -18,10 +18,27 @@ namespace ASP_Homework_Product.Controllers
             _logger = logger;
         }
 
+        List<Product> products = new List<Product>()
+        {
+            new Product("Коленвал", 15000, "Коленчатый вал для Mercedes-Benz E500"),
+            new Product("Поршни", 12000, "Облегченная поршневая система"),
+            new Product("Бензонасос", 9000, "Улчушенный бензонасос")
+        };
         public string Index()
         {
-            ProductStorage product = new ProductStorage();
-            return string.Join("\n\n", product.ShowProducts());
+            var result = "";
+            foreach (var item in products)
+            {
+                result += item + "\n\n";
+            }
+            return result;
+
+
+
+            /*ProductStorage product = new ProductStorage();
+            return string.Join("\n\n", product.ShowProducts());*/
+
+
             //return product.ShowProducts();
 
 

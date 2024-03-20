fbefef236d52901c4dcad0dcd8ec62d19bd1e956
George Gussaov <gussaov04@gmail.com>
2024-03-20 22:16:45 +0300

    Реализована неработающая панель администратора.

---
+++ ASP_Homework_Product/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Homework_Product.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Orders()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult Roles()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}

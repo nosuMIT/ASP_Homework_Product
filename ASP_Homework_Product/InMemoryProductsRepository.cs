4bd91aca9c386d305a04496315c6177ca9bdc1d0
GioevArsen <gioev.arsen15@list.ru>
2024-03-25 23:45:50 +0300

    Already added earlier(ability to increase/decrease each position in the cart)

--- ASP_Homework_Product/InMemoryProductsRepository.cs
+++ ASP_Homework_Product/InMemoryProductsRepository.cs
@@ -11,7 +11,7 @@ namespace ASP_Homework_Product
         List<Product> products = new List<Product>
         {
             new Product("Elden ring", Convert.ToDecimal(49.89), "Epic adventure set in a world of dark fantasy and mysteries, where players explore a vast open world, battle colossal enemies, and uncover the secrets of ancient history, created in collaboration between Hidetaka Miyazaki and George R. R. Martin.", "/images/elden-ring.jpg"),
-            new Product("Minecraft", Convert.ToDecimal(29.99), "Sandbox game where players can explore, create, and survive in a blocky, procedurally-generated world, limited only by their imagination.", "/images/minecraft.jpg"),
+            new Product("Minecraft", Convert.ToDecimal(29.99), "Sandbox game where players can explore, create, and survive in a blocky, procedurally-generated world, limited only by their imagination!", "/images/minecraft.jpg"),
             new Product("Cyberpunk 2077", Convert.ToDecimal(35.49), "Open-world action RPG set in the dystopian future of Night City, offering players a gritty narrative-driven experience with deep character customization and immersive cybernetic enhancements.", "/images/cyberpunk-2077.jpg"),
             new Product("Lethal Company", Convert.ToDecimal(4.85), "A co-op horror about scavenging at abandoned moons to sell scrap to the Company.", "/images/lethal-company.jpg"),
             new Product("Valheim", Convert.ToDecimal(5.39), "Cooperative survival game set in a procedurally-generated world inspired by Norse mythology, where players must gather resources, build shelters, craft equipment, and battle mythical creatures to prove their worth to the gods.", "/images/valheim.jpg"),
